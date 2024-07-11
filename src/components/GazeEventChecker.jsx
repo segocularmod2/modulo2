3b56370a7093fb91a992acd3e76372267451ee50
hlsalazar <hlsalazar@espe.edu.ec>
2024-07-10 19:58:15 -0500

    funcionalidad parcial , lectura is ok

--- src/components/GazeEventChecker.jsx
+++ src/components/GazeEventChecker.jsx
@@ -1,22 +1,23 @@
 // GazeEventChecker.jsx
 import React from 'react';
-import useGazeEvent from './useGazeEvent'; // Asumiendo que los datos se pasan como props o están en contexto/global state
 import Movies, { data } from './DataFetcher.tsx';
+import useGazeEvent from './useGazeEvent'; // Asumiendo que los datos se pasan como props o están en contexto/global state
 
-const gazeevents_old = [
+const gazeevents = [
   { docX: 100, docY: 100, time: 1718751444024, state: 0, duration: 33 },
   { docX: 120, docY: 120, time: 1718751449727, state: 0, duration: 123 },
   { docX: 621, docY: 384, time: 1718751451000, state: 0, duration: 150 },
   // Más puntos simulados según necesites
 ];
 
-const puntos2 = data;
-const puntos = data.slice(0, 3); // Tomamos solo los tres primeros elementos
-
 
 const GazeEventChecker = () => {
 
-  const { clickPositions, matchingEventsArray, message } = useGazeEvent(puntos);
+  const { clickPositions, matchingEventsArray, message } = useGazeEvent(gazeevents);
+
+
+  const puntos2 = data;
+  const puntos = data.slice(0, 3); // Tomamos solo los tres primeros elementos
 
   return (
     <div>
@@ -29,7 +30,6 @@ const GazeEventChecker = () => {
       {matchingEventsArray.map((event, index) => (
         <div key={index}>Evento {index + 1}: X={event.docX}, Y={event.docY}</div>
       ))}
-
       <pre>{JSON.stringify(puntos, null, 2)}</pre>
 
     </div>
