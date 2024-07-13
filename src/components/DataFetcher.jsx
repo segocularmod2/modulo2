573fbc805e3aac06611f06e855d71e21269e3b3f
hlsalazar <hlsalazar@espe.edu.ec>
2024-07-13 15:10:39 -0500

    arreglo extension datafetcher

--- src/components/DataFetcher.jsx
+++ src/components/DataFetcher.jsx
@@ -1,4 +1,5 @@
-import type { FunctionalComponent } from 'preact';
+//import type { FunctionalComponent } from 'preact';
+
 
 const data = await fetch('https://api-seguim-ocular.vercel.app/api/data').then((response) =>
   response.json()
@@ -8,11 +9,5 @@ const data = await fetch('https://api-seguim-ocular.vercel.app/api/data').then((
 // When rendered with a client:* directive, they also log to the browser console.
 console.log(data);
 
-const Movies: FunctionalComponent = () => {
-  // Output the result to the page
-  return <div>{JSON.stringify(data)}</div>;
-};
 
-export default Movies;
-export { data }; // Export the data for use in App.jsx
-
+export default data; // Export the data for use in App.jsx
